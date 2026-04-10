711cd18364fe8215323106a38d38469db4776fc2
113Rizky <rizky75338@gmail.com>
2026-04-10 19:55:48 +0800

    Posttest 4

---
+++ Posttest4/src/PS4.java
@@ -0,0 +1,11 @@
+public class PS4 extends PlayStation {
+
+    PS4() {
+        super("PS4", 10000);
+    }
+
+    @Override
+    public String getInfoKonsol() {
+        return "PS4 cocok untuk rental hemat dan game populer.";
+    }
+}
---
+++ Posttest4/src/PS5.java
@@ -0,0 +1,11 @@
+public class PS5 extends PlayStation {
+
+    PS5() {
+        super("PS5", 15000);
+    }
+
+    @Override
+    public String getInfoKonsol() {
+        return "PS5 cocok untuk performa tinggi dan game generasi terbaru.";
+    }
+}
